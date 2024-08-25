d8bc79395c7d591b108db65813fcfd22266c7dba
Zenay83 <151439571+Zenay83@users.noreply.github.com>
2024-08-25 15:04:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+    header.addEventListener('click', function() {
+        const content = this.nextElementSibling;
+        
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+});
